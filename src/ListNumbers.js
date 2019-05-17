c14146da55b4c2fd31cb68213235e384710e1116
Latence132 <alexandre.chaumet@gadz.org>
2019-05-17 21:32:41 +0200

    materlize.css added

--- src/ListNumbers.js
+++ src/ListNumbers.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function ListNumbers(props) {
   // Correct ! Pas la peine de spécifier la clé ici :
-  let tab = props.numbers.map(number => <li>{number}</li>);
-  return <ul>{tab}</ul>;
+  let tab = props.numbers.map(number => (
+    <li className="collection-item">{number}</li>
+  ));
+
+  return <ul className="collection">{tab}</ul>;
 }
 
 export default ListNumbers;
--- src/Toggle.js
+++ src/Toggle.js
@@ -1,29 +1,29 @@
-import React from 'react';
+import React from "react";
 
+import { Button } from "react-materialize";
 class Toggle extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {isToggleOn: true};
-  
-      // Cette liaison est nécéssaire afin de permettre
-      // l'utilisation de `this` dans la fonction de rappel.
-      this.handleClick = this.handleClick.bind(this);
-    }
-  
-    handleClick() {
-      this.setState(state => ({
-        isToggleOn: !state.isToggleOn
-      }));
-    }
-  
-    render() {
-      return (
-        <button onClick={this.handleClick}>
-          {this.state.isToggleOn ? 'ON' : 'OFF'}
-        </button>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: true };
+
+    // Cette liaison est nécéssaire afin de permettre
+    // l'utilisation de `this` dans la fonction de rappel.
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  export default Toggle
-  
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <Button onClick={this.handleClick}>
+        {this.state.isToggleOn ? "ON" : "OFF"}
+      </Button>
+    );
+  }
+}
+
+export default Toggle;
